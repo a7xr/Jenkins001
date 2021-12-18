38f46c687520eaca8e8caabf16936cb3351fe5b2
Mamitiana Solofo <solofomamitiana0@gmail.com>
2021-12-18 11:44:36 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Modified 20211218_005");
+        System.out.println("Modified 20211218_006");
     }
 }
